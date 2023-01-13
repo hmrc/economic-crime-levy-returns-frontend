119659370c1568e6f58262fd7d5705bc0cc3ca64
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2023-01-13 11:26:55 +0000

    Update jsoup

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-28"   % "7.12.0",
-    "org.jsoup"            % "jsoup"                    % "1.14.3",
+    "org.jsoup"            % "jsoup"                    % "1.15.3",
     "org.mockito"         %% "mockito-scala"            % "1.17.12",
     "org.scalatestplus"   %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola" %% "random-data-generator"    % "2.9"
