e8e7d9b559a82db23b51ea7efc84ef43c2af5354
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2023-04-12 13:57:41 +0100

    ECL-321: Initial commit

--- app/uk/gov/hmrc/economiccrimelevyreturns/controllers/StartController.scala
+++ app/uk/gov/hmrc/economiccrimelevyreturns/controllers/StartController.scala
@@ -17,29 +17,73 @@
 package uk.gov.hmrc.economiccrimelevyreturns.controllers
 
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import uk.gov.hmrc.economiccrimelevyreturns.connectors.{EclAccountConnector, EclReturnsConnector}
 import uk.gov.hmrc.economiccrimelevyreturns.controllers.actions.AuthorisedAction
-import uk.gov.hmrc.economiccrimelevyreturns.services.EnrolmentStoreProxyService
+import uk.gov.hmrc.economiccrimelevyreturns.models.requests.AuthorisedRequest
+import uk.gov.hmrc.economiccrimelevyreturns.models.{Fulfilled, ObligationData, ObligationDetails, Open}
+import uk.gov.hmrc.economiccrimelevyreturns.services.{EclReturnsService, EnrolmentStoreProxyService}
 import uk.gov.hmrc.economiccrimelevyreturns.views.ViewUtils
 import uk.gov.hmrc.economiccrimelevyreturns.views.html.StartView
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class StartController @Inject() (
   val controllerComponents: MessagesControllerComponents,
   authorise: AuthorisedAction,
   enrolmentStoreProxyService: EnrolmentStoreProxyService,
+  eclAccountConnector: EclAccountConnector,
+  eclReturnsService: EclReturnsService,
+  eclReturnsConnector: EclReturnsConnector,
   view: StartView
 )(implicit ec: ExecutionContext)
     extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = authorise.async { implicit request =>
-    enrolmentStoreProxyService.getEclRegistrationDate(request.eclRegistrationReference).map { registrationDate =>
-      Ok(view(request.eclRegistrationReference, ViewUtils.formatLocalDate(registrationDate)))
+  def onPageLoad(periodKey: String): Action[AnyContent] = authorise.async { implicit request =>
+    for {
+      registrationDate        <- enrolmentStoreProxyService.getEclRegistrationDate(request.eclRegistrationReference)
+      obligationData          <- eclAccountConnector.getObligations()
+      eitherObligationDetails <- validatePeriodKey(obligationData, periodKey)
+    } yield eitherObligationDetails match {
+      case Right(obligationDetails) =>
+        Ok(
+          view(
+            request.eclRegistrationReference,
+            ViewUtils.formatLocalDate(registrationDate),
+            ViewUtils.formatObligationPeriodYears(obligationDetails)
+          )
+        )
+      case Left(result)             => result
+    }
+  }
+
+  private def validatePeriodKey(obligationData: Option[ObligationData], periodKey: String)(implicit
+    request: AuthorisedRequest[_]
+  ): Future[Either[Result, ObligationDetails]] = {
+    val obligationDetails =
+      obligationData
+        .map(_.obligations.flatMap(_.obligationDetails.find(_.periodKey == periodKey)))
+        .flatMap(_.headOption)
+
+    obligationDetails match {
+      case None                    => Future.successful(Left(Ok("No obligation details found for period"))) //TODO handle this properly
+      case Some(obligationDetails) =>
+        obligationDetails.status match {
+          case Fulfilled =>
+            Future.successful(
+              Left(Ok(s"Obligation for period $periodKey already fulfilled"))
+            ) //TODO handle this properly
+          case Open      =>
+            for {
+              eclReturn <- eclReturnsService.getOrCreateReturn(request.internalId)
+              _         <-
+                eclReturnsConnector.upsertReturn(eclReturn.copy(obligationDetails = Some(obligationDetails)))
+            } yield Right(obligationDetails)
+        }
     }
   }
 
