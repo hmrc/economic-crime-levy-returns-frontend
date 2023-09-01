0b710c66335eba48f1b606337964698af088c336
liandra-giacomine <91252116+liandra-giacomine@users.noreply.github.com>
2023-09-01 09:13:55 +0100

    ECL-462: Replace £ with html code (#91)
    * ECL-462: Replace £ with html code
    
    * Use formatMoney rather than formatNumber for UkRevenue row

--- app/uk/gov/hmrc/economiccrimelevyreturns/viewmodels/checkanswers/UkRevenueSummary.scala
+++ app/uk/gov/hmrc/economiccrimelevyreturns/viewmodels/checkanswers/UkRevenueSummary.scala
@@ -33,7 +33,7 @@ object UkRevenueSummary {
     request.eclReturn.relevantApRevenue.map { answer =>
       SummaryListRowViewModel(
         key = Key("checkYourAnswers.ukRevenue.label"),
-        value = ValueViewModel(HtmlContent(HtmlFormat.escape(s"£${ViewUtils.formatNumber(answer)}"))),
+        value = ValueViewModel(HtmlContent(HtmlFormat.escape(s"£${ViewUtils.formatMoney(answer)}"))),
         actions = Seq(
           ActionItemViewModel("site.change", routes.UkRevenueController.onPageLoad(CheckMode).url)
             .withVisuallyHiddenText(
