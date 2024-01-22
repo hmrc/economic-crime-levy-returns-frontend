ab97b03d132c45bcaf1179ffc73f19ea69542ec0
Liandra Giacomine <liandra11@hotmail.com>
2024-01-22 15:33:41 +0000

    ECL-622: Updated generator for stringsLongerThan function

--- test-common/uk/gov/hmrc/economiccrimelevyreturns/generators/Generators.scala
+++ test-common/uk/gov/hmrc/economiccrimelevyreturns/generators/Generators.scala
@@ -128,7 +128,7 @@ trait Generators {
   def stringsLongerThan(minLength: Int): Gen[String] = for {
     maxLength <- (minLength * 2).max(100)
     length    <- Gen.chooseNum(minLength + 1, maxLength)
-    chars     <- listOfN(length, arbitrary[Char])
+    chars     <- listOfN(length, Gen.alphaNumChar)
   } yield chars.mkString
 
   def stringsExceptSpecificValues(excluded: Seq[String]): Gen[String] =
