8d692d975e369305dce4bb6cad2f4ccf2f643827
Patrick Lucas <patrick.lucas@opencastsoftware.com>
2024-03-15 11:28:04 +0000

    ECL-618: Improve tests

--- it/uk/gov/hmrc/economiccrimelevyreturns/ContactEmailISpec.scala
+++ it/uk/gov/hmrc/economiccrimelevyreturns/ContactEmailISpec.scala
@@ -12,7 +12,7 @@ import uk.gov.hmrc.economiccrimelevyreturns.models.{CheckMode, EclReturn, Normal
 class ContactEmailISpec extends ISpecBase with AuthorisedBehaviour {
 
   def updateContactEmail(eclReturn: EclReturn, email: String) =
-    eclReturn.copy(contactEmailAddress = Some(email))
+    eclReturn.copy(contactEmailAddress = Some(email.toLowerCase))
 
   def clearContactEmail(eclReturn: EclReturn) =
     eclReturn.copy(contactEmailAddress = None)
@@ -27,60 +27,60 @@ class ContactEmailISpec extends ISpecBase with AuthorisedBehaviour {
     updateContactName(eclReturn)
   }
 
-//  s"GET ${routes.ContactEmailController.onPageLoad(NormalMode).url}" should {
-//    behave like authorisedActionRoute(routes.ContactEmailController.onPageLoad(NormalMode))
-//
-//    "respond with 200 status and the contact email HTML view" in {
-//      stubAuthorised()
-//
-//      val eclReturn        = random[EclReturn]
-//      val name             = random[String]
-//      val sessionData      = random[SessionData]
-//      val validSessionData = sessionData.copy(values = Map(SessionKeys.PeriodKey -> testPeriodKey))
-//
-//      stubGetReturn(eclReturn.copy(contactName = Some(name)))
-//      stubGetSession(validSessionData)
-//      stubUpsertSession()
-//
-//      val result = callRoute(FakeRequest(routes.ContactEmailController.onPageLoad(NormalMode)))
-//
-//      status(result) shouldBe OK
-//
-//      html(result) should include(s"What is $name's email address?")
-//    }
-//  }
-//
-//  s"POST ${routes.ContactEmailController.onSubmit(NormalMode).url}" should {
-//    behave like authorisedActionRoute(routes.ContactEmailController.onSubmit(NormalMode))
-//
-//    "save the provided email address then redirect to the contact telephone number page" in {
-//      stubAuthorised()
-//
-//      val eclReturn        = random[EclReturn]
-//      val name             = random[String]
-//      val email            = emailAddress(MinMaxValues.EmailMaxLength).sample.get
-//      val sessionData      = random[SessionData]
-//      val validSessionData = sessionData.copy(values = Map(SessionKeys.PeriodKey -> testPeriodKey))
-//
-//      val updatedReturn = eclReturn.copy(contactName = Some(name), contactEmailAddress = Some(email.toLowerCase))
-//
-//      stubGetReturn(updatedReturn)
-//      stubUpsertReturn(updatedReturn)
-//      stubGetSession(validSessionData)
-//
-//      val result = callRoute(
-//        FakeRequest(routes.ContactEmailController.onSubmit(NormalMode))
-//          .withFormUrlEncodedBody(("value", email))
-//      )
-//
-//      status(result) shouldBe SEE_OTHER
-//
-//      redirectLocation(result) shouldBe Some(routes.ContactNumberController.onPageLoad(NormalMode).url)
-//    }
-//  }
+  s"GET ${routes.ContactEmailController.onPageLoad(NormalMode).url}" should {
+    behave like authorisedActionRoute(routes.ContactEmailController.onPageLoad(NormalMode))
+
+    "respond with 200 status and the contact email HTML view" in {
+      stubAuthorised()
+
+      val eclReturn        = random[EclReturn]
+      val name             = random[String]
+      val sessionData      = random[SessionData]
+      val validSessionData = sessionData.copy(values = Map(SessionKeys.PeriodKey -> testPeriodKey))
+
+      stubGetReturn(eclReturn.copy(contactName = Some(name)))
+      stubGetSession(validSessionData)
+      stubUpsertSession()
+
+      val result = callRoute(FakeRequest(routes.ContactEmailController.onPageLoad(NormalMode)))
+
+      status(result) shouldBe OK
+
+      html(result) should include(s"What is $name's email address?")
+    }
+  }
+
+  s"POST ${routes.ContactEmailController.onSubmit(NormalMode).url}" should {
+    behave like authorisedActionRoute(routes.ContactEmailController.onSubmit(NormalMode))
+
+    "save the provided email address then redirect to the contact telephone number page" in {
+      stubAuthorised()
+
+      val eclReturn        = random[EclReturn]
+      val name             = random[String]
+      val email            = emailAddress(MinMaxValues.EmailMaxLength).sample.get
+      val sessionData      = random[SessionData]
+      val validSessionData = sessionData.copy(values = Map(SessionKeys.PeriodKey -> testPeriodKey))
+
+      val updatedReturn = eclReturn.copy(contactName = Some(name), contactEmailAddress = Some(email.toLowerCase))
+
+      stubGetReturn(updatedReturn)
+      stubUpsertReturn(updatedReturn)
+      stubGetSession(validSessionData)
+
+      val result = callRoute(
+        FakeRequest(routes.ContactEmailController.onSubmit(NormalMode))
+          .withFormUrlEncodedBody(("value", email))
+      )
+
+      status(result) shouldBe SEE_OTHER
+
+      redirectLocation(result) shouldBe Some(routes.ContactNumberController.onPageLoad(NormalMode).url)
+    }
+  }
 
   s"POST ${routes.ContactEmailController.onSubmit(CheckMode).url}" should {
-//    behave like authorisedActionRoute(routes.ContactEmailController.onSubmit(CheckMode))
+    behave like authorisedActionRoute(routes.ContactEmailController.onSubmit(CheckMode))
     behave like goToNextPageInCheckMode(
       value = emailAddress(MinMaxValues.EmailMaxLength).sample.get,
       updateEclReturnValue = updateContactEmail,
