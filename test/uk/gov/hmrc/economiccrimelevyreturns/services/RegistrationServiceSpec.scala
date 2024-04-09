f4fedccaf3e4bdfcebfc788c19af16610ba055e0
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-04-09 11:24:17 +0100

    ECL-769: Corrected test error (#167)
    * ECL-769: Corrected test error
    
    * ECL-769: Removed unused imports
    
    * ECL-769: Updated play bootstrap library and autobuild plugin
    
    * ECL-769: Updated sbt plugin

--- test/uk/gov/hmrc/economiccrimelevyreturns/services/RegistrationServiceSpec.scala
+++ test/uk/gov/hmrc/economiccrimelevyreturns/services/RegistrationServiceSpec.scala
@@ -21,8 +21,6 @@ import org.mockito.ArgumentMatchers.any
 import play.api.http.Status.INTERNAL_SERVER_ERROR
 import uk.gov.hmrc.economiccrimelevyreturns.connectors.RegistrationConnector
 import uk.gov.hmrc.economiccrimelevyreturns.generators.CachedArbitraries._
-import uk.gov.hmrc.economiccrimelevyreturns.models.errors.DataHandlingError
-import uk.gov.hmrc.economiccrimelevyreturns.models.GetSubscriptionResponse
 import uk.gov.hmrc.economiccrimelevyreturns.models.GetSubscriptionResponse
 import uk.gov.hmrc.economiccrimelevyreturns.models.errors.DataHandlingError
 import uk.gov.hmrc.http.UpstreamErrorResponse
