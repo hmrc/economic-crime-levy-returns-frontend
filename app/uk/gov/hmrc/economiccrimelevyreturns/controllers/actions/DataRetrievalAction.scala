7fe65ac13a5b9afc73485c3cfc9dfad3f09e5b16
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-04-18 14:35:32 +0100

    ECL-899: Corrected race condition in data retrieval actions
    Co-Authored-By: Liam O'Connor <74724858+liamoc89@users.noreply.github.com>

--- app/uk/gov/hmrc/economiccrimelevyreturns/controllers/actions/DataRetrievalAction.scala
+++ app/uk/gov/hmrc/economiccrimelevyreturns/controllers/actions/DataRetrievalAction.scala
@@ -112,20 +112,18 @@ class ReturnDataRetrievalAction @Inject() (
     implicit request: AuthorisedRequest[_]
   ): EitherT[Future, DataHandlingError, EclReturn] = {
     val optPeriodKey = eclReturn.obligationDetails.map(_.periodKey)
-    if (optPeriodKey.contains(periodKey) || optPeriodKey.isEmpty) {
-      val updatedReturn =
-        eclReturn.copy(obligationDetails = Some(obligationDetails), returnType = Some(FirstTimeReturn))
-      eclReturnService.upsertReturn(updatedReturn)
-      EitherT.fromEither[Future](Right(updatedReturn))
-    } else {
-      for {
-        _            <- eclReturnService.deleteReturn(request.internalId)
-        updatedReturn = EclReturn
+
+    for {
+      _            <- eclReturnService.deleteReturn(request.internalId)
+      updatedReturn = if (optPeriodKey.contains(periodKey) || optPeriodKey.isEmpty) {
+                        eclReturn.copy(obligationDetails = Some(obligationDetails), returnType = Some(FirstTimeReturn))
+                      } else {
+                        EclReturn
                           .empty(request.internalId, None)
                           .copy(obligationDetails = Some(obligationDetails), returnType = Some(FirstTimeReturn))
-        _            <- eclReturnService.upsertReturn(updatedReturn)
-      } yield updatedReturn
-    }
+                      }
+      _            <- eclReturnService.upsertReturn(updatedReturn)
+    } yield updatedReturn
   }
 
   private def getStartAmendUrl(returnType: Option[ReturnType], session: Session, internalId: String)(implicit
