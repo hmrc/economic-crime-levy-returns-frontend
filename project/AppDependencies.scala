b1656c9c64c1daa1198b5212cc5918c36f6c4fc7
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2025-07-17 15:23:45 +0100

    DDCYLS-6747: Scala FMT Formatting

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,12 +11,12 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-test-play-30"   % hmrcBootstrapVersion,
-    "org.jsoup"             % "jsoup"                    % "1.21.1",
-    "org.mockito"          %% "mockito-scala"            % "2.0.0",
-    "org.scalatestplus"    %% "scalacheck-1-17"          % "3.2.18.0",
-    "com.danielasfregola"  %% "random-data-generator"    % "2.9",
-    "io.github.wolfendale" %% "scalacheck-gen-regexp"    % "1.1.0"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30" % hmrcBootstrapVersion,
+    "org.jsoup"             % "jsoup"                  % "1.21.1",
+    "org.mockito"          %% "mockito-scala"          % "2.0.0",
+    "org.scalatestplus"    %% "scalacheck-1-17"        % "3.2.18.0",
+    "com.danielasfregola"  %% "random-data-generator"  % "2.9",
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
