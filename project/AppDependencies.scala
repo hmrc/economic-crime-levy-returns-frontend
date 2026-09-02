a3f42f1aae4e8beed1cd2042bb77e8235e9684c8
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-09-02 13:58:18 +0100

    updating dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "13.9.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "13.11.0",
     "org.typelevel" %% "cats-core"                  % "2.13.0"
   )
 
